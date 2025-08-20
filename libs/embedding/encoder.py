b215d5eadb0c5649d4ffc0b1cc49b1242bf13133
anaskras <ankrasnoshchekova@gmail.com>
2025-08-20 15:57:28 +0300

    encoder fix

--- libs/embedding/encoder.py
+++ libs/embedding/encoder.py
@@ -3,6 +3,7 @@ from sentence_transformers import SentenceTransformer
 
 _model = None
 
+
 def get_model():
     global _model
     if _model is None:
@@ -10,10 +11,14 @@ def get_model():
         _model = SentenceTransformer(name)
     return _model
 
+
 def encode_texts(texts: list[str]) -> list[list[float]]:
-    m = get_model()
-    vecs = m.encode(texts, normalize_embeddings=True).tolist()
-    return vecs
+    model = get_model()
+    vectors = model.encode(texts, normalize_embeddings=True).tolist()
+    return vectors
+
 
 def dim() -> int:
     return get_model().get_sentence_embedding_dimension()
+
+
